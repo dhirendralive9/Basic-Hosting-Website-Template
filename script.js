34a093a978deb05f9a1d1b85564f887061e76045
Dhirendra Biswal <dhirendrabiswal9@live.com>
2022-04-02 23:57:18 +0530

    added animations , bugs need to be fixed

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ var selectPlanButtons = document.querySelectorAll('.plan button');
 var toggleButton = document.querySelector('.toggle-button');
 var mobileNav = document.querySelector('.mobile-nav');
 var modalNo = document.querySelector('.modal__action--negative');
+var ctaButton = document.querySelector('.main-nav__item--cta');
 
 console.log(backdrop);
 
@@ -13,13 +14,24 @@ selectPlanButtons.forEach(x => {
    x.addEventListener('click',()=>{
          
          modal.classList.add('open');
-         backdrop.classList.add('open');
+         backdrop.style.display = 'block';
+         setTimeout(()=>{
+            backdrop.classList.add('open');
+        },10);
    });
 })
 
   function closeModel()  {
-    modal.classList.remove('open');
+     if(modal){
+         modal.classList.remove("open");
+         }
          backdrop.classList.remove('open');
+        
+         setTimeout(function(){
+            backdrop.style.display = 'none';
+        },200);
+
+
     };
 
 
@@ -36,5 +48,21 @@ modalNo.addEventListener('click',()=>{
 
 toggleButton.addEventListener('click',()=>{
     mobileNav.classList.add('open');
- backdrop.classList.add('open');
+    backdrop.style.display = 'block';
+    setTimeout(()=>{
+        backdrop.classList.add('open');
+    },10);
+
+});
+
+ctaButton.addEventListener('animationstart',(e)=>{
+    
+});
+
+ctaButton.addEventListener('animationend',(e)=>{
+    
+});
+
+ctaButton.addEventListener('animationiteration',(e)=>{
+    
 });
